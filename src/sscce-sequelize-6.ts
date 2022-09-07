7924d54e934001aa66484b92727c74e3c42f99b5
wkrasnicki-vizlib <wiktor.krasnicki@vizlib.com>
2022-09-07 14:42:49 +0200

    Create a simple test with connection string params

--- src/sscce-sequelize-6.ts
+++ src/sscce-sequelize-6.ts
@@ -12,15 +12,22 @@ export const testingOnDialects = new Set(["postgres"]);
 export async function run() {
   // This function should be used instead of `new Sequelize()`.
   // It applies the config for your SSCCE to work on CI.
-  const sequelize = createSequelize6Instance({
+  const sequelizeObject = createSequelize6Instance({
+    database: "sequelize test",
     logQueryParameters: true,
     benchmark: true,
     define: {
       // For less clutter in the SSCCE
       timestamps: false,
     },
-    logging: false,
   });
 
+  // ? I modified this method to accept connection strings
+  const sequelizeConnString = createSequelize6Instance(
+    "postgres://sequelize_test:sequelize_test@localhost:5432/sequelize test",
+  );
+
   // You can use sinon and chai assertions directly in your SSCCE.
+  expect(sequelizeObject.authenticate()).to.eventually.fulfilled;
+  expect(sequelizeConnString.authenticate()).to.eventually.fulfilled;
 }
